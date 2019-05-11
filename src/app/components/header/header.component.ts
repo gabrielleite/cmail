1854610ca86127f46e768a60a5f93b51a8491ec9
Gabriel Leite <gabrielcfcleite@gmail.com>
2019-05-11 04:28:52 -0300

    [Exercício] Css e HeaderComponent.

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cmail-header',
+    templateUrl: './header.component.html',
+    styleUrls: [
+        './header.component.css',
+        './header-search.css'
+    ]
+})
+export class HeaderComponent {}
